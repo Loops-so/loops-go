d612638d08cea71b567423fb5d41ec20ceea1ce8
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2026-05-05 07:02:41 -0700

    components api (#3)

---
+++ components.go
@@ -0,0 +1,78 @@
+package loops
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+type Component struct {
+	ComponentID string `json:"componentId"`
+	Name        string `json:"name"`
+	LMX         string `json:"lmx"`
+}
+
+func (c *Client) GetComponent(id string) (*Component, error) {
+	req, err := c.newRequest(http.MethodGet, "/components/"+id, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errorFromResponse(resp)
+	}
+
+	var result Component
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return &result, nil
+}
+
+func (c *Client) ListComponents(params PaginationParams) ([]Component, *Pagination, error) {
+	q := url.Values{}
+	if params.PerPage != "" {
+		q.Set("perPage", params.PerPage)
+	}
+	if params.Cursor != "" {
+		q.Set("cursor", params.Cursor)
+	}
+
+	path := "/components"
+	if len(q) > 0 {
+		path += "?" + q.Encode()
+	}
+
+	req, err := c.newRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resp, err := c.do(req)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, nil, errorFromResponse(resp)
+	}
+
+	var result struct {
+		Pagination Pagination  `json:"pagination"`
+		Data       []Component `json:"data"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, nil, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return result.Data, &result.Pagination, nil
+}
